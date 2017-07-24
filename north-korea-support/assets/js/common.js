447d1bbccca7d18907337c231057c2d65fcbde49
designens <inyoung524@naver.com>
2017-07-24 10:30:31 +0900

    인크루드 분리, 아코디언 메뉴 수정

--- north-korea-support/assets/js/common.js
+++ north-korea-support/assets/js/common.js
@@ -58,36 +58,10 @@
        $('#gnb-all-menu').toggleClass('open');
     });
 
-    // LNB 메뉴
-    $(function() {
-        var Accordion = function(el, active) {
-            this.el = el || {};
-            active = active || 0;
-            var that = this;
-            var links = this.el.find('.lnb-link');
-            links.each(function(i){
-                var link = links.eq(i);
-                if (link.next().length === 0) { link.find('.fa').hide(); }
-                link.on('click', { link: link }, that.dropdown);
-            });
-            if (active > 0) {
-               links.eq(active - 1).trigger('click');     
-            }
-        }
-        // 아코디언 설정
-        Accordion.prototype.dropdown = function(e) {
-            e.preventDefault();
-            var $this = e.data.link;
-            $this.parent()
-                .siblings('.open').find('.submenu').slideUp()
-            .addBack().removeClass('open');
-            $this.parent()
-                .toggleClass('open')
-                .find('.submenu').stop().slideToggle();
-        };
+    // 아코디언 설정
+    var accordion = new $.Accordion($('.accordion-lnb'), 1), // LNB 메뉴
+        accordion_faq = new $.Accordion($('.accordion-faq'), 1); // QnA 메뉴
 
-        var accordion = new Accordion($('#accordion'), 1);
-        // 첫번째 메뉴오픈
-    });
+
 
 })(window, document, window.jQuery);
--- north-korea-support/assets/js/lib-js.js
+++ north-korea-support/assets/js/lib-js.js
@@ -298,3 +298,32 @@ window.smoothScroll = (function (window, document, undefined) {
 	};
 
 })(window, document);
+;(function($) {
+
+    $.Accordion = function(el, active) {
+        this.el = el || {};
+        active = active || 0;
+        var that = this;
+        var links = this.el.find('.lnb-link');
+        links.each(function(i){
+            var link = links.eq(i);
+            if (link.next().length === 0) { link.find('.fa').hide(); }
+            link.on('click', { link: link }, that.dropdown);
+        });
+        if (active > 0) {
+           links.eq(active - 1).trigger('click');     
+        }
+    }
+    // 아코디언 설정
+    $.Accordion.prototype.dropdown = function(e) {
+        e.preventDefault();
+        var $this = e.data.link;
+        $this.parent()
+            .siblings('.open').find('.submenu').slideUp()
+        .addBack().removeClass('open');
+        $this.parent()
+            .toggleClass('open')
+            .find('.submenu').stop().slideToggle();
+    };
+    
+}(window.jQuery));
