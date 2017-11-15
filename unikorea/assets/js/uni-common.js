1a7852c853d06f6b59a5eea0ecf5b6b28228a914
designens <inyoung524@naver.com>
2017-11-15 17:58:37 +0900

    marc js & jquery css 제거, Mylibrary > 대출/연장 > 리스트, Mylibrary > 반납 > 리스트 완료
    Mylibrary > 대출/연장 > 리스트완료
    html/loans-extensions-reservations-list.html
    Mylibrary > 반납 > 리스트완료
    html/loans-return-reservations-list.html
    marc js & jquery css 제거

--- unikorea/assets/js/uni-common.js
+++ unikorea/assets/js/uni-common.js
@@ -337,21 +337,21 @@
 // =================================================================
 // 소장정보 상세보기/MARC보기 실행
 // =================================================================
-(function(global, $) {
+// (function(global, $) {
 
-    var $btn_marc_view = $(".btn-marc-view"),
-        list_view_area = ".list-view-area",
-        marc_view_area = ".marc-view-area",
-        open_text = "상세보기",
-        close_text = "MARC 보기";
+//     var $btn_marc_view = $(".btn-marc-view"),
+//         list_view_area = ".list-view-area",
+//         marc_view_area = ".marc-view-area",
+//         open_text = "상세보기",
+//         close_text = "MARC 보기";
     
-    $btn_marc_view.click(function() {
-        var $this = $(this);
-        if($this.parent().siblings(list_view_area).css("display") == "none" )
-            $this.html(close_text).parent().siblings(list_view_area).show(),
-             $this.parent().siblings(marc_view_area).hide();
-        else $this.html(open_text).parent().siblings(marc_view_area).show(),
-            $this.parent().siblings(list_view_area).hide();
-    });
+//     $btn_marc_view.click(function() {
+//         var $this = $(this);
+//         if($this.parent().siblings(list_view_area).css("display") == "none" )
+//             $this.html(close_text).parent().siblings(list_view_area).show(),
+//              $this.parent().siblings(marc_view_area).hide();
+//         else $this.html(open_text).parent().siblings(marc_view_area).show(),
+//             $this.parent().siblings(list_view_area).hide();
+//     });
 
-})(window, window.jQuery);
+// })(window, window.jQuery);
